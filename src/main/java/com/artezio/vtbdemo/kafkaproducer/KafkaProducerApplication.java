c085fcf0ae8087a1a613023acc40b667e4b88a08
Roman Padalka <rpadalka@Romans-Mac-mini.local>
2020-11-17 22:48:56 +0300

    Initial commit

---
+++ src/main/java/com/artezio/vtbdemo/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.artezio.vtbdemo.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
